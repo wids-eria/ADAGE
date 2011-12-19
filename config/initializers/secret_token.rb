b24228209296fad1cf5061d0821b816b84e0c330
Kevin Alford <zeroeth@pixelflow.org>
2011-12-19 12:53:55 -0600

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ada::Application.config.secret_token = 'beabaad8f551904a73e6195734ae5f4af2aae017e3d3185fe7bdc4c7326bd1fbc3b8ded5f715edcb9286c32294194dba90fdfe4bb75a8f2fb27b4f763012749b'
