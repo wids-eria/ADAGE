f9dc05573b76e273da95c1d6a22c1070ae7678b8
Kevin Alford <zeroeth@pixelflow.org>
2011-12-22 16:38:32 -0600

    Remove controller auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, :except => [:authenticate_for_token]
   skip_authorization_check :only => :authenticate_for_token
 
   def authenticate_for_token
