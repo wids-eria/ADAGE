2d17111322e1d78389b8e9aaf6aa407ff9965248
Kevin Alford <zeroeth@pixelflow.org>
2011-12-22 16:34:57 -0600

    Add token auth from trailsforward

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!, :except => [:authenticate_for_token]
+  skip_authorization_check :only => :authenticate_for_token
+
+  def authenticate_for_token
+    @user = User.find_by_email params[:email]
+    if @user != nil and @user.valid_password? params[:password]
+      @auth_token = @user.authentication_token
+      ret = {:session_id => @user.id, :auth_token => @auth_token}
+      respond_to do |format|
+        format.json {render :json => ret }
+        format.xml  {render :xml => ret }
+      end
+    else
+      render :nothing => true, :status => :forbidden
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Ada::Application.routes.draw do
   get "welcome/index"
 
   devise_for :users
+  match "/users/authenticate_for_token" => "users#authenticate_for_token"
 
   root :to => 'welcome#index'
 end
