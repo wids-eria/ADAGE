9609262c67b5c7455fcdea353611274f839a2563
Kevin Alford <zeroeth@pixelflow.org>
2011-12-23 12:30:44 -0600

    Make authenticate for token behave like devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
   def authenticate_for_token
     @user = User.find_by_email params[:email]
+    ret = {}
     if @user != nil and @user.valid_password? params[:password]
       @auth_token = @user.authentication_token
       ret = {:session_id => @user.id.to_s, :auth_token => @auth_token}
       respond_to do |format|
-        format.json {render :json => ret }
-        format.xml  {render :xml => ret }
+        format.json {render :json => ret, :status => :created }
+        format.xml  {render :xml => ret, :status => :created }
       end
     else
-      render :nothing => true, :status => :forbidden
+      ret = {:errors => "Invalid email or password"}
+      respond_to do |format|
+        format.json {render :json => ret, :status => :unauthorized }
+        format.xml  {render :xml => ret, :status => :unauthorized }
+      end
     end
   end
 end
