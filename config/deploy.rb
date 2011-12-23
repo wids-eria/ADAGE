a1fd6136f8f3692797212e24c5cf71c480b5bb95
Kevin Alford <zeroeth@pixelflow.org>
2011-12-23 10:40:29 -0600

    Add passenger restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,4 +35,14 @@ namespace :deploy do
   task :symlink_db, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
+
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
 end
