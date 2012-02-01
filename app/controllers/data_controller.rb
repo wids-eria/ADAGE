af7b2a3b67da4c21288c78313c3d1d6d6bcafc73
Kevin Alford <zeroeth@pixelflow.org>
2012-02-01 15:13:09 -0600

    Test authentication to data controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,4 +1,5 @@
 class DataController < ApplicationController
+  before_filter :authenticate_user!
   respond_to :html, :json
 
   def index
--- spec/controllers/data_controller_spec.rb
+++ spec/controllers/data_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe DataController do
   let!(:user) { Fabricate :user, password: 'pass1234' }
 
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in :user, user
+  end
+
   describe "#index" do
     let(:datum) { create :datum }
     it "returns a list of data" do
@@ -15,7 +20,7 @@ describe DataController do
 
     it "creates mongo records from incoming json" do
       lambda do
-        post :create, "data" => [{"one" => 1}, {"two" => 2}], "format" => "json" 
+        post :create, "data" => [{"one" => 1}, {"two" => 2}], "format" => "json"
         response.status.should be(201)
       end.should change(AdaData, :count).by(2)
     end
