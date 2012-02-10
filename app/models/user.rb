842b9260537bc2e4f0fdc2b741e059aa4d7afeea
Change Me <change@me.org>
2012-02-10 13:03:40 -0600

    Remove broken association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :authentication_token
-  has_many :ada_datas
 end
