91470c3a99829d822897e67d3c0403726d8b4645
Kevin Alford <zeroeth@pixelflow.org>
2012-05-08 10:53:15 -0500

    Info level logger

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,6 +19,8 @@ Ada::Application.configure do
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
+  config.log_level = :info
+
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
