4273820887e16e80bca71c2cfb811f3653e28657
Allison Salmon and Kevin Alford <dev+allisonsalmon+zeroeth@learninggamesnetwork.org>
2012-11-28 15:40:47 -0600

    Reorganize so ada and tf deploy files are in similar order

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,23 @@
+set :rvm_ruby_string, '1.9.3'
 set :rvm_type, :system
 
-set :rvm_ruby_string, '1.9.3'
+require 'bundler/capistrano'
+load 'deploy/assets'
 
 require 'capistrano/ext/multistage'
 set :stages, %w(production staging)
 set :default_stage, "staging"
 
-require 'bundler/capistrano'
-load 'deploy/assets'
-
 set :application, "ada"
-
-set :scm, :git
 set :repository,  "git@github.com:wids-eria/ada.git"
+set :scm, :git
 
 set :user, :deploy
+ssh_options[:forward_agent] = true
+
 set :deploy_to, "/home/deploy/applications/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
-ssh_options[:forward_agent] = true
 
 set :normalize_asset_timestamps, false
 
