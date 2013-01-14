9265a3cc239cefce241b1f37939deeb4ed57d5c6
asalmon <allisonmariesalmon@gmail.com>
2013-01-14 09:33:20 -0600

    show some basic averages for fun

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,14 @@ class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     @users = User.select{ |user| user.data.where(gameName: @game.name).count > 0 }
+    @average_time = 0
+    if @users.count > 0
+      @users.each do |user| 
+          @average_time +=  user.data.last.created_at - user.data.first.created_at
+      end
+      @average_time = @average_time/@users.count
+    end
+
   end
 
   def index
