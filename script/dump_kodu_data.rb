d710d714fb7e4301115ce916530948f19f2e3ee3
asalmon <allisonmariesalmon@gmail.com>
2013-01-16 10:41:13 -0600

    kodu script tweak

--- script/dump_kodu_data.rb
+++ script/dump_kodu_data.rb
@@ -18,14 +18,14 @@ end
 =end
 
 
-CSV.open("kodu_moving_minds_data.csv", "w") do |csv|
+CSV.open("kodu_1_14_2013.csv", "w") do |csv|
   csv << ['player name', 'created at', 'timestamp', 'tag name', 'data']
   puts players.count
-    data = AdaData.where(gameName: 'kodu', user_id: 3454..3456)
+    data = AdaData.where(gameName: 'kodu', consented: true)
     puts data.count
     bar = ProgressBar.new 'wee', data.count 
     data.each do |log_entry|
-      csv << [players.detect{|p| p.id == log_entry.user_id}.id, log_entry.created_at, log_entry.time, log_entry.name, log_entry.data]
+      csv << [players.detect{|p| p.id == log_entry.user_id}.email, log_entry.created_at, log_entry.time, log_entry.name, log_entry.data]
       bar.inc
     end
     bar.finish
