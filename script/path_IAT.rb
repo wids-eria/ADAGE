853af56518f0dd079c5a16d4203b20a562a4f794
asalmon <allisonmariesalmon@gmail.com>
2013-02-12 11:17:00 -0600

    output the IAT results to a csv

--- script/path_IAT.rb
+++ script/path_IAT.rb
@@ -1,12 +1,19 @@
+require 'csv'
+
 players=User.all
-players.each do |play|
-  data = play.data.where(gameName: 'FairPlay')
-  if data.count > 0
-    data.each do |log_entry|
-      if log_entry.key == 'IATFinalBias'
-        puts play.player_name + " " + play.authentication_token + " " + log_entry.bias.to_s + " " + play.control_group.to_s
+
+CSV.open("fairplay_IAT.csv", "w") do |csv|
+  csv << ['player name', 'auth token', 'final bias', 'control group']
+  players.each do |play|
+    data = play.data.where(gameName: 'FairPlay')
+    if data.count > 0
+      data.each do |log_entry|
+        if log_entry.key == 'IATFinalBias'
+          puts play.player_name + " " + play.authentication_token + " " + log_entry.bias.to_s + " " + play.control_group.to_s
+          csv << [play.player_name, play.authentication_token, log_entry.bias.to_s, play.control_group.to_s]
+        end
       end
+      
     end
-    
   end
 end
