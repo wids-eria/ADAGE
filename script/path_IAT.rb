12f720e0e673131a2e04e8531cb3a9f4c791a762
asalmon <allisonmariesalmon@gmail.com>
2013-03-09 12:42:57 -0600

    out put the delta times and std deviations for the first run of the IAT

--- script/path_IAT.rb
+++ script/path_IAT.rb
@@ -2,18 +2,24 @@ require 'csv'
 
 players=User.all
 
-CSV.open("fairplay_IAT.csv", "w") do |csv|
-  csv << ['player name', 'auth token', 'final bias', 'control group']
+CSV.open("csv/fairplay/fairplay_IAT.csv", "w") do |csv|
+  csv << ['player name', 'auth token', 'final bias', 'control group'] + ['delta times']*4 +  ['standard deviations']*2
   players.each do |play|
-    data = play.data.where(gameName: 'FairPlay')
+    data = play.data.where(gameName: 'FairPlay', schema: '12-14-2012 Belinda Data 1')
     if data.count > 0
-      data.each do |log_entry|
-        if log_entry.key == 'IATFinalBias'
-          puts play.player_name + " " + play.authentication_token + " " + log_entry.bias.to_s + " " + play.control_group.to_s
-          csv << [play.player_name, play.authentication_token, log_entry.bias.to_s, play.control_group.to_s]
-        end
-      end
+      puts play.player_name
+      final_bias = data.where(key: 'IATFinalBias').first
+      if final_bias != nil
+        delta_times = data.where(key: 'IATDeltaTimes').limit(4).map{ |x| x.deltaTime }
+        puts delta_times.count
+        std_devs = data.where(key: 'IATStandardDeviations').limit(2).map{ |x| x.deviation }
+        puts std_devs.count
       
+        padding = ['']*(20 - delta_times.count)
+
+        csv << [play.player_name, play.authentication_token, final_bias.bias, play.control_group] + delta_times + std_devs      
+      end
+
     end
   end
 end
