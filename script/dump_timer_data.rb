dedcf9bdb6c4c86ef228283a9b500177d5a9858c
asalmon <allisonmariesalmon@gmail.com>
2013-06-25 13:35:17 -0500

    script for app timer data and tweaks to other tenacity scripts

--- script/dump_timer_data.rb
+++ script/dump_timer_data.rb
@@ -1,16 +1,16 @@
 require 'csv'
 require 'progressbar'
 
-players=User.all.select{|u| u.data.where(gameName: 'Tenacity-Meditation').count > 0 }
+players=User.all.select{|u| u.data.where(gameName: 'App Timer').count > 0 }
 #players = User.where(player_name: 'TM1')
 puts 'total players ' + players.count.to_s
 #grab all the column headers
-types = AdaData.where(gameName: 'Tenacity-Meditation').distinct(:key)
+types = AdaData.where(gameName: 'App Timer').distinct(:key)
 puts types.inspect
 examples = Array.new
 bar = ProgressBar.new 'gathering types', types.count
 types.each do |type|
-  ex = AdaData.where(key: type).first
+  ex = AdaData.first
   if ex != nil
     examples << ex
   end
@@ -32,21 +32,27 @@ puts all_attrs.inspect
 
 #bar = ProgressBar.new 'parsing players', players.count
 players.each do |play|
-  data = play.data.where(gameName: 'Tenacity-Meditation')
+  data = play.data.where(gameName: 'App Timer')
   if data.count > 0
-    CSV.open("csv/meditation/tenacity_test_"+play.email+".csv", "w") do |csv|
+    CSV.open("csv/timer/tenacity_timer_"+play.email+".csv", "w") do |csv|
+      csv << all_attrs.uniq
       data.each do |entry|
-        out = Array.new
-        all_attrs.uniq.each do |attr|
-          if entry.attributes.keys.include?(attr)
-            out << entry.attributes[attr]
-          else
-            out << ""
+        unless entry.key.include?('KoKPlayerMovement')
+          out = Array.new
+          all_attrs.uniq.each do |attr|
+            if entry.attributes.keys.include?(attr)
+              out << entry.attributes[attr]
+            else
+              out << ""
+            end
           end
+          csv << out
         end
-        csv << out
       end
     end
   end
+  bar.inc
+
+ # bar.inc
 end
 #bar.finish
