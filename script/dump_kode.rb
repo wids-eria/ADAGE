100409b24e0516d764fe35e8d9f6a19fcbd039d5
Isaac Goodin <isaacgoodin@gmail.com>
2013-09-19 14:06:59 -0500

    Removed progressbar require import

--- script/dump_kode.rb
+++ script/dump_kode.rb
@@ -1,4 +1,4 @@
-require 'progressbar'
+#require 'progressbar'
 require 'json'
 require 'csv'
 
@@ -6,15 +6,15 @@ class Student
   attr_accessor :user
 
   def initialize(user)
-    self.user = user 
+    self.user = user
   end
-  
+
   def run csv
-    
+
     #find all the kode entries
     kode_logs = user.data.where(gameName: 'kodu')
     if kode_logs.count > 0
-      #puts user.player_name + " has " + kode_logs.count.to_s + " kode logs" 
+      #puts user.player_name + " has " + kode_logs.count.to_s + " kode logs"
       #parse the kode to json
       kodes = Array.new
       kode_logs.each do |log|
@@ -23,7 +23,7 @@ class Student
 
       #puts user.player_name + " successfully parsed!"
     end
-  
+
   end
 
 end
@@ -32,7 +32,7 @@ class AnalyizeKode
 
   def run name, students
     #bar = ProgressBar.new 'students', students.count
-    csv = CSV.open("csv/kodu/"+name+".csv", "w") 
+    csv = CSV.open("script/csv/kodu/"+name+".csv", "w")
     students.each do |student_name|
       user = User.where(["lower(player_name) = :login", login: student_name.first.downcase]).first
       if user != nil
@@ -49,11 +49,11 @@ class AnalyizeKode
 
 end
 
-sparta = CSV.open("csv/kodu/sections/sparta.csv", 'r')
-peagle = CSV.open("csv/kodu/sections/peagle.csv", 'r')
-glacialD = CSV.open("csv/kodu/sections/glacialD.csv", 'r')
-waunakee = CSV.open("csv/kodu/sections/waunakee.csv", 'r')
+sparta = CSV.open("script/csv/kodu/sections/sparta.csv", 'r')
+peagle = CSV.open("script/csv/kodu/sections/peagle.csv", 'r')
+glacialD = CSV.open("script/csv/kodu/sections/glacialD.csv", 'r')
+waunakee = CSV.open("script/csv/kodu/sections/waunakee.csv", 'r')
 AnalyizeKode.new.run 'Sparta_kode_dump', sparta
 AnalyizeKode.new.run 'palmyra-eagle_kode_dump', peagle
 AnalyizeKode.new.run 'glacial_drummlin_kode_dump', glacialD
-AnalyizeKode.new.run 'waunakee_kode_dump', waunakee 
+AnalyizeKode.new.run 'waunakee_kode_dump', waunakee
