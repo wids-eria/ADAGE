3fff8950c57275886992f4e1ce69a8b53614baee
asalmon <allisonmariesalmon@gmail.com>
2013-10-09 14:05:27 -0500

    fix the simlink and try the real url for dev server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,7 @@ Ada::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'ada.dev.eriainteractive.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
