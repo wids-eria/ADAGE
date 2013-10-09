54bd04e9c97fee079e6b30115e838cd27a1c5a7e
asalmon <allisonmariesalmon@gmail.com>
2013-10-09 11:49:27 -0500

    hook up devise action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,16 @@ Ada::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'ada.dev.eriainteractive.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
