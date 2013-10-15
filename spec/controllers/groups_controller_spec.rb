88cd177d97f219016d5ff4075d4f324453cc6555
Isaac Goodin <isaacgoodin@gmail.com>
2013-10-15 11:16:29 -0500

    Added a few basic test for group

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -5,27 +5,23 @@ describe GroupsController do
   let!(:admin_role) { Role.create(name: 'admin') }
   let!(:user) { Fabricate :user, password: 'pass1234', roles: [Role.where(name: 'admin').first, Role.where(name: 'teacher').first] }
 
-
   before do
     @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in :user, user
   end
 
   describe "#index" do
-    let(:group) { Fabricate :groups }
     it "returns a list of groups" do
       get :index
       response.should be_success
     end
   end
-=begin
+
   describe "#show" do
-    let(:group) { Fabricate :group }
+    let(:group) { Fabricate :group ,name: "Play Squad"}
     it "returns group and a list of users" do
-      puts group.inspect
       get :show, :id => group.id
       response.should be_success
     end
   end
-=end
 end
