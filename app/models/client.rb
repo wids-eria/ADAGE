a9dca1288d8709c017c697c74568f06d3fa6cb3d
asalmon <allisonmariesalmon@gmail.com>
2013-10-15 14:05:36 -0500

    fix and test methods of authenticating before posting data

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,9 @@
 class Client < ActiveRecord::Base
+  before_create :generate_tokens
+
+  def generate_tokens
+    self.app_token = SecureRandom.hex(16)
+    self.app_secret = SecureRandom.hex(32)
+  end
+
 end
