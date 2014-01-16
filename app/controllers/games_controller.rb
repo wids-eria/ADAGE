00b889a05c3a0eebc2bbd12682951c45b8538703
Isaac Goodin <isaacgoodin@gmail.com>
2014-01-16 11:38:16 -0600

    Tweaked num_users query to only query for user_id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -23,7 +23,7 @@ class GamesController < ApplicationController
   def statistics
     @game = Game.find(params[:id])
     @log_count = AdaData.with_game(@game.name).only(:_id).count
-    @num_users = AdaData.with_game(@game.name).distinct(:user_id).count
+    @num_users = AdaData.with_game(@game.name).only(:user_id).distinct(:user_id).count
   end
 
   def sessions
