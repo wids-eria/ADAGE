bbf02c0c9c412eca0c67cd7c277b2a187a97ea0e
Isaac Goodin <isaacgoodin@gmail.com>
2014-01-31 11:05:36 -0600

    Fixed a bug

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -52,9 +52,9 @@ class DataController < ApplicationController
     @session_count = 0
 
     #Check for the ADAVersion for compatability before all the processing
-    log = AdaData.with_game(@game.name).only(:_id,:ADAVersion).where(:ADAVersion.exists=>true).first
-    if log
-      drunken_dolphin = log.ADAVersion.include?('drunken_dolphin')
+    version = AdaData.with_game(@game.name).only(:_id,:ADAVersion).where(:ADAVersion.exists=>true).first.ADAVersion
+    if version
+      drunken_dolphin = version.include?('drunken_dolphin')
       logs = AdaData.with_game(@game.name).order_by(:timestamp.asc).in(user_id: params[:user_ids]).only(:ADAVersion,:timestamp,:user_id,:session_token).where(:ADAVersion.exists=>true).map_reduce(map,reduce).out(inline:1)
 
       sessions_played = 0
