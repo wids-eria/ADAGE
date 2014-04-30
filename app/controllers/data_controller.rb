3a1c74d0ccdcac3d5ab699639a14a1897b2990ab
asalmon <allisonmariesalmon@gmail.com>
2014-04-30 13:53:54 -0500

    try to filter the data by the users in the game

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -187,6 +187,9 @@ class DataController < ApplicationController
 
       unless @graph_params.game_id.nil? or @graph_params.game_id.empty?
         @url = @url + '&game_id=' + @graph_params.game_id
+
+        @user_ids = AdaData.with_game(@game.name).where(game_id: @graph_params.game_id).distinct(:user_id)
+        @url = @url + '&user_ids='
       end
       
       @keys = AdaData.with_game(@game.name).distinct(:key)
