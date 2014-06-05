68ae33f55602d94881bd1c9527ad0b5fa09e025e
Isaac Goodin <isaacgoodin@gmail.com>
2014-06-05 11:08:48 -0500

    Added optional playlogin code for new user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
-class SessionsController < ApplicationController
-  skip_authorization_check
-
+class SessionsController < Devise::SessionsController
 
   def create
-
+    super
+    current_user.add_to_group(params[:user][:group])
   end
 
+  def new
+    @code = params[:code]
+  end
 end
