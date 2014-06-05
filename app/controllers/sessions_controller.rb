995ba58debe689c4f6022273191b859815cca8b5
Isaac Goodin <isaacgoodin@gmail.com>
2014-06-05 17:07:06 -0500

    Added session parameter to denote wither or not to show code login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < Devise::SessionsController
   def create
     super
     current_user.add_to_group(params[:user][:group])
+
+    session[:portal] = false
   end
 
   def new
-    @code = params[:code]
+    @code = session[:portal]
   end
 end
