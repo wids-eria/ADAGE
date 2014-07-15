ca93a2519e11c77a878b46c86abd4b7d8899bb0e
Isaac Goodin <isaacgoodin@gmail.com>
2014-07-15 15:38:57 -0500

    Dashboard styling

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,20 +3,13 @@ $(document).ready(function () {
     $(".graph").each(function(g){
         var data = [{data:[]}];
         var graph = new Rickshaw.Graph( {
-            element: this,
-            width: 400,
-            height: 250,
-            series: data,
-            min: 'auto',
-            interpolation: 'linear',
-            renderer: 'line'
-        });
-
-        var xAxis = new Rickshaw.Graph.Axis.Time({
-            graph: graph
-        });
-        var yAxis = new Rickshaw.Graph.Axis.Y({
-            graph: graph
+          element: this,
+          width: 400,
+          height: 250,
+          series: data,
+          min: 'auto',
+          interpolation: 'linear',
+          renderer: 'line',
         });
 
 
@@ -25,18 +18,26 @@ $(document).ready(function () {
           data[0].data = response['data'];
           data[0].color = 'steelblue';
           graph.update();
-          xAxis.render();
-          yAxis.render();
 
           var hoverDetail = new Rickshaw.Graph.HoverDetail( {
             graph: graph,
           });
 
           var legend = new Rickshaw.Graph.Legend( {
-        graph: graph,
-        element: document.getElementById('legend')
+            graph: graph,
+            element: document.getElementById('legend')
+          });
 
-      } );
+          var xAxis = new Rickshaw.Graph.Axis.Time({
+            graph: graph,
+            orientation: 'bottom',
+          });
+
+          var yAxis = new Rickshaw.Graph.Axis.Y({
+            graph: graph,
+          });
+          yAxis.render();
+          xAxis.render();
 
         })
         .fail(function() {})
