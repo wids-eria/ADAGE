1d87abc5ec91ebcd2a2bf07d7c59d9eb6dd9a371
Isaac Goodin <isaacgoodin@gmail.com>
2014-07-16 14:55:48 -0500

    Graph styling

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,48 +1,49 @@
 $(document).ready(function () {
-
-    $(".graph").each(function(g){
-        var data = [{data:[]}];
-        var graph = new Rickshaw.Graph( {
-          element: this,
-          width: 400,
-          height: 250,
-          series: data,
-          min: 'auto',
-          interpolation: 'linear',
-          renderer: 'line',
-        });
-
-
-        var jqxhr = $.get( "remote_graph.json", function(response) {
-          data[0].name = response['name'];
-          data[0].data = response['data'];
-          data[0].color = 'steelblue';
-          graph.update();
-
-          var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-            graph: graph,
-          });
-
-          var legend = new Rickshaw.Graph.Legend( {
-            graph: graph,
-            element: document.getElementById('legend')
-          });
-
-          var xAxis = new Rickshaw.Graph.Axis.Time({
-            graph: graph,
-            orientation: 'bottom',
-          });
-
-          var yAxis = new Rickshaw.Graph.Axis.Y({
-            graph: graph,
-          });
-          yAxis.render();
-          xAxis.render();
-
-        })
-        .fail(function() {})
-        .always(function() {});
-
+  $(".graph").each(function(g){
+    var self = this;
+    var data = [{data:[]}];
+    var graph = new Rickshaw.Graph( {
+      element: this,
+      width: 380,
+      height: 250,
+      series: data,
+      min: 'auto',
+      interpolation: 'linear',
+      renderer: 'line',
+      padding: { top: 0.08, right: 0, bottom: 0.06, left: 0 },
     });
 
+    var jqxhr = $.get( "remote_graph.json", function(response) {
+      data[0].name = response['name'];
+      data[0].data = response['data'];
+      data[0].color = 'steelblue';
+      graph.update();
+
+      var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        graph: graph,
+        orientation: 'left',
+      });
+
+      var legend = new Rickshaw.Graph.Legend( {
+        graph: graph,
+        element: $(self).parent().find("#legend")[0]
+      });
+
+      var xAxis = new Rickshaw.Graph.Axis.Time({
+        graph: graph,
+        orientation: 'bottom',
+      });
+
+      var yAxis = new Rickshaw.Graph.Axis.Y({
+        graph: graph,
+        element: $(self).parent().find("#yaxis")[0]
+      });
+      yAxis.render();
+      xAxis.render();
+
+    })
+    .fail(function() {})
+    .always(function() {});
+
+  });
 });
