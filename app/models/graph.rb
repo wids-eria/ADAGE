6e21cec7e2d7263204df864b30a5a1fe341f27f9
Isaac Goodin <isaacgoodin@gmail.com>
2014-07-17 14:54:20 -0500

    Graph filters no db driven

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -1,5 +1,6 @@
 class Graph < ActiveRecord::Base
   belongs_to :dashboard
   attr_accessible :dashboard, :settings, :metrics
+  alias_attribute :filters, :settings
   validates :dashboard, presence: true
 end
