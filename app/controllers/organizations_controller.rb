66181eaf1d49261b9bc0f80f8cdb41a4643636cb
Isaac Goodin <isaacgoodin@gmail.com>
2015-07-27 11:11:19 -0500

    Added Management views for organizations

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,42 @@
+class OrganizationsController < ApplicationController
+  load_and_authorize_resource
+  skip_authorize_resource :only => :playsquads
+  before_filter :authenticate_user!
+  layout 'blank'
+
+  def index
+    @orgs = Organization.all
+  end
+
+  def show
+    @org = Organization.find(params[:id])
+  end
+
+  def edit
+    @org = Organization.find(params[:id])
+  end
+
+  def update
+    @org = Organization.find(params[:id])
+    if @org.update_attributes params[:organization]
+      flash[:notice] = 'organization Updated'
+      redirect_to @org
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @org = Organization.new(params[:organization])
+  end
+
+  def create
+    @org = Organization.new(params[:organization])
+    if @org.save
+      flash[:notice] = 'organization Added'
+      redirect_to @org
+    else
+      render :new
+    end
+  end
+end
